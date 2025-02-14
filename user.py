58439e7436916873bb5e54c7d7172fdcb21f3783
unknown <karkisugam34@gmail.com>
2025-02-14 11:02:35 +0545

    extra users

--- user.py
+++ user.py
@@ -4,21 +4,16 @@ import time
 print("Loading Users ...")
 
 class User:
-    def __init__(self, username, password, name):
+    def __init__(self, username, password, name, is_admin):
         self.username = username
         self.password = password
         self.name = name 
-        self.balance = {asset.arr[0][9]: 10000 for asset in assets} # for demo-user, using 10,000 shares as their balance for each asset in the market
+        self.balance = {asset.arr[0][9]: 100000 for asset in assets} # for demo-user, using 1,00,000 shares as their balance for each asset in the market
         self.collateral = 1000000.00 # for demo-user, 10 lakhs as their purchasing power
+        self.is_admin = is_admin
 
     def check_password(self, password):
         return self.password == password
-
-    def update_balance(self, amount):
-        if self.balance + amount >= 0:  # Ensure balance doesn't go negative
-            self.balance += amount
-            return True
-        return False
 
 
 # Simulating a database of users
@@ -26,6 +21,35 @@ users = {
     "oic_finance": User(
         "oic_finance",
         "iamuser",
-        "Orchid International"
+        "Orchid International",
+        False
+        ),
+
+    "sugam_karki": User(
+        "sugam_karki",
+        "iamuser",
+        "Sugam Karki",
+        False
+        ),
+
+    "anish_tamang": User(
+        "anish_tamang",
+        "iamuser",
+        "Anish Tamang",
+        False
+        ),
+
+    "parag_niraula": User(
+        "parag_niraula",
+        "iamuser",
+        "Parag Niraula",
+        False
+        ),
+        
+    "admin": User(
+        "admin",
+        "iamadmin",
+        "Administrator",
+        True
         )
 }
